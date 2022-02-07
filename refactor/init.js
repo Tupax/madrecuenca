1d2f6e9239e3d44190e473ecdccc94ba6765e6e0
tupax <tupaxmaro@gmail.com>
2022-02-07 18:13:30 -0300

    map funcionando

---
+++ refactor/init.js
@@ -0,0 +1,43 @@
+class Task {
+    constructor(name,Deadline,Status) {
+        this.name = name;
+        this.Deadline = Deadline;
+        this.Status = Status;
+        // this.recipiente = parseInt(recipiente);
+      }
+}
+
+const dataAirtable = [];
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keym0Wh3AN1Yv63Sv'}).base('appZWKwzBefWAYvqk');
+
+base('Tasks').select({
+    // Selecting the first 3 records in All tasks:
+    maxRecords: 3,
+    view: "All tasks",
+    fields: ["Name","Deadline","Status"]
+}).eachPage(function page(records, fetchNextPage) {
+    // This function (`page`) will get called for each page of records.
+
+
+    try {
+
+    dataAirtable.push (records.map( db => new Task(db.get('Name'),null,db.get('Deadline'),db.get('Status')) ));
+
+    // To fetch the next page of records, call `fetchNextPage`.
+    // If there are more records, `page` will get called again.
+    // If there are no more records, `done` will get called.
+    }
+    catch(e){ console.log('error inside eachPage => ',e)}
+    fetchNextPage();
+    
+
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+
+
+    console.log(dataAirtable);
+});
+
+
---
+++ refactor/readme.md
@@ -0,0 +1,21 @@
+# Refactor and learning project
+
+## Algunos cambios
+
+- Un usuario especifico para probar cosas en airtable
+
+
+    API KEY : keym0Wh3AN1Yv63Sv
+    BASE ID : appZWKwzBefWAYvqk
+    Creative agency proposal planning
+    The ID of this base is appZWKwzBefWAYvqk. 
+
+- un usario para probar cosas en otras plataformas similares 
+
+- Un usuario especifico para probar cosas en webflow 
+
+
+
+## Usar la funcion map
+
+
